6cbe33d05979feb9d8536bdc0bff2f805654bed3
Eder Bevacqua <ebevacqua@viajanetsp.local>
2019-11-06 18:53:24 -0300

    melhorias Front

--- equipamentos_api.py
+++ equipamentos_api.py
@@ -24,39 +24,44 @@ def cadastrar():
         novo_equipamento = {"numeroEquipamento":request.form["numeroEquipamento"], "marca" :request.form["marca"], "modelo" : request.form["modelo"], "status" : request.form["status"]}
         equipamento = service_criar(novo_equipamento)
         if equipamento == None:
-            return render_template("index.html", equipamentos=service_listar(), mensagem = "Equipamento não pôde ser cadastrado! \n")
+            return render_template("index.html", equipamentos=service_listar(), mensagem = "Equipamento nao pode ser cadastrado! \n")
         else:
-            return render_template('index.html', equipamentos=service_listar(), mensagem='Cadastrado')
+            return render_template('index.html', equipamentos=service_listar(), mensagem='Equipamento cadastrado')
     return render_template("cadastrar.html")
 
 @equipamentos_app.route('/equipamentos/localizar', methods=['POST','GET'])
 def localizar():
-    if request.form and request.method == 'POST':
-        numEquipamento = request.form["numeroEquipamento"]
-        equipamento = service_localiza(numEquipamento)
-        if equipamento != None:
-            return render_template("localizar.html", equipamento=equipamento)
-        else:
-            return render_template("localizar.html", mensagem="Equipamento nao encontrado")
-    return render_template("localizar.html")
+    try:
+        if request.form and request.method == 'POST':
+            numEquipamento = request.form["numeroEquipamento"]
+            equipamento = service_localiza(numEquipamento)
+            if equipamento != None:
+                return render_template("localizar.html", equipamento=equipamento)
+            else:
+                return render_template("localizar.html", mensagem="Equipamento nao encontrado")
+        return render_template("localizar.html")
+    except ValueError:
+        return render_template("localizar.html", mensagem="Digite o NUMERO do equipamento")
 
 @equipamentos_app.route('/equipamentos/editar', methods=['GET','POST'])
 def editar():
-    if request.form and request.method == 'POST':
-        numEquipamento = request.form["numeroEquipamento"]
-        equipamento = service_localiza(numEquipamento)
-        return render_template("editar.html", equipamento=equipamento)
-    return render_template("editar.html")
- 
+    try:
+        if request.form and request.method == 'POST':
+            numEquipamento = request.form["numeroEquipamento"]
+            equipamento = service_localiza(numEquipamento)
+            if equipamento != None:
+                return render_template("editar.html", equipamento=equipamento)
+            else:
+                return render_template("editar.html", mensagem="Equipamento nao encontrado")
+        return render_template("editar.html")
+    except ValueError:
+        return render_template("editar.html", mensagem="Digite o NUMERO do equipamento")
+
 @equipamentos_app.route('/equipamentos/atualizar/<int:numeroEquipamento>', methods=['POST'])
 def alterar_equipamento(numeroEquipamento):
     marca=request.form.get("marca")
     modelo=request.form.get("modelo")
     status=request.form.get("status")
-    if marca == None:
-        return render_template("index.html", mensagem='erro, equipamento sem marca')
-    if modelo==None:
-        return render_template("index.html", mensagem='erro, equipamento sem modelo')
     atualizado = service_atualiza(numeroEquipamento, marca, modelo,status)
     if atualizado != None:
         return render_template("index.html", equipamentos=service_listar(), mensagem='Alteracao efetuada com sucesso')
@@ -64,11 +69,17 @@ def alterar_equipamento(numeroEquipamento):
     
 @equipamentos_app.route('/equipamentos/excluir', methods=['POST','GET'])
 def excluir():
-    if request.form and request.method == 'POST':
-        numEquipamento = request.form["numeroEquipamento"]
-        equipamento = service_localiza(numEquipamento)
-        return render_template("excluir.html", equipamento=equipamento)
-    return render_template("excluir.html")
+    try:
+        if request.form and request.method == 'POST':
+            numEquipamento = request.form["numeroEquipamento"]
+            equipamento = service_localiza(numEquipamento)
+            if equipamento != None:
+                return render_template("excluir.html", equipamento=equipamento)
+            else:
+                return render_template("excluir.html", mensagem="Equipamento nao encontrado")
+        return render_template("excluir.html")
+    except ValueError:
+        return render_template("excluir.html", mensagem="Digite o NUMERO do equipamento")
 
 @equipamentos_app.route('/equipamentos/remover/<int:numeroEquipamento>', methods=['POST'])
 def remover_equipamento(numeroEquipamento):
